fcff4f19dcae988bbd1ec4864491ac9c8e1bcecd
Miguel <miguel.castineira@gmail.com>
2019-02-07 10:40:03 -0300

    Persona 2.0  7/2 persona.py

--- Dino.py
+++ Dino.py
@@ -18,8 +18,27 @@ pepito2 = Dino("Pepo", "rojo")
 pepito3 = Dino("Pepx", "rosa")
 
 
+#4to ejercicio  
+class Dino:
+    def __init__(self,un_nombre="",un_color=""):
+        self.nombre = un_nombre
+        self.color = un_color
+        print("Hola soy un dinosaurio, me llamo", self.nombre,"y soy de color", self.color)
 
+    def rugir(self,rugido):
+        #print(self)
+        #print(rugido)
+        print("RRRRWWAAAR!!!", rugido)
 
+    def cambiar_color(self, un_color):
+        self.color= un_color
 
+pepito = Dino("Pepe", "azul")
 
-pepo= Dino(un_color="Pink", un_nombre="Floyd")
+pepito.cambiar_color("Lila")
+
+pepito.rugir("AHHHHHHH!!!!!!!!")
+
+pepito1 = Dino("Pepu", "naranaja")
+pepito2 = Dino("Pepo", "rojo")
+pepito3 = Dino("Pepx", "rosa")
