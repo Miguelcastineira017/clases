bf231c63593cc641a043df1466a39e570b092b0b
Miguel <miguel.castineira@gmail.com>
2019-02-07 10:42:50 -0300

    Persona 2.0 7 /2 pesona.py

--- persona.py
+++ persona.py
@@ -7,12 +7,23 @@
 
 class Personal:
 
-    def __init__(self, un_nombre="",un_edad="",un_profesion=""):
+    def __init__(self, un_nombre=" ", un_edad="", un_profesion=" "):
         self.nombre = un_nombre
         self.edad = un_edad
         self.profesion = un_profesion
         print("Mi nombre es", self.nombre, "tengo", self.edad, "y soy", self.profesion)
+
+    def cumpleanhos(self):
+        self.edad = self.edad + 1
+        print(self.edad)
     
-persona1 = Personal("Emilio", 30, "Doctor.")
-persona2 = Personal("Jose", 40, "Cientifico.")
-persona3 = Personal("Roberto", 50, "Astronauta.")
+persona1 = Personal("Emilio", 33, "Doctor.")
+persona2 = Personal("Jose", 48, "Cientifico.")
+persona3 = Personal("Roberto", 51, "Astronauta.")
+
+# Agregar un metodo(self)a la clase persona, que se llame cumpleanhos
+# y aumente la edad de la persona en un anho y retorna la edad
+
+persona1.cumpleanhos()
+persona2.cumpleanhos()
+persona3.cumpleanhos()
